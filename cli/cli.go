d347123e39067905310ba8ed054366dc0d2e4d00
Anton Novojilov <andy@essentialkaos.com>
2024-10-21 14:53:33 +0300

    Fix compatibility with the latest version of ek package

--- cli/cli.go
+++ cli/cli.go
@@ -159,7 +159,7 @@ func Run(gitRev string, gomod []byte) {
 
 	if !errs.IsEmpty() {
 		terminal.Error("Options parsing errors:")
-		terminal.Error(errs.String())
+		terminal.Error(errs.Error("- "))
 		os.Exit(1)
 	}
 
